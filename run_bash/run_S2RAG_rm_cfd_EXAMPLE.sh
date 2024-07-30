e87b40e434dfc4af4298fd13f27f5f59b1f9d2ac
minqixiang <minqixiang@tencent.com>
2024-07-30 21:35:33 +0100

    env setup on mac

--- run_bash/run_S2RAG_rm_cfd_EXAMPLE.sh
+++ run_bash/run_S2RAG_rm_cfd_EXAMPLE.sh
@@ -1,7 +1,7 @@
 export CUDA_VISIBLE_DEVICES=0
 
-python /home/minqi/code/S2RAG/scripts/run_S2RAG_ablation_rm_cfd.py \
---model_name /home/minqi/code/S2RAG/model/Llama-2-7b-chat-hf \
+python scripts/run_S2RAG_ablation_rm_cfd.py \
+--model_name model/Llama-2-7b-chat-hf \
 --tau 0.45 \
---bertmodel /home/minqi/code/S2RAG/model/en_core_web_sm-3.6.0/en_core_web_sm/en_core_web_sm-3.6.0 \
+--bertmodel model/en_core_web_sm-3.6.0/en_core_web_sm/en_core_web_sm-3.6.0 \
 --choice 0 
--- run_bash/run_selfadpt_EXAMPLE.sh
+++ run_bash/run_selfadpt_EXAMPLE.sh
@@ -1,7 +1,7 @@
 export CUDA_VISIBLE_DEVICES=0
 
-python /home/minqi/code/S2RAG/scripts/selfadpt_collection.py \
---model_name /home/minqi/code/S2RAG/model/Llama-2-7b-chat-hf \
---input_file /home/minqi/code/S2RAG/data_eval/popqa_longtail_w_gs_example.jsonl \
---output_file /home/minqi/code/S2RAG/minqi_inf_output/llama2chat-pqa-selfadp.json \
+python scripts/selfadpt_collection.py \
+--model_name model/Llama-2-7b-chat-hf \
+--input_file data_eval/popqa_longtail_w_gs_example.jsonl \
+--output_file minqi_inf_output/llama2chat-pqa-selfadp.json \
 --max_new_tokens 20
